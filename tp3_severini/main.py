20a11006ab4dd9ab3828f120868bf6b7e51fcc1c
Nicolas Severini <nicoseve823@gmail.com>
2023-11-21 21:55:15 -0300

    tp3_Severini

--- tp3_severini/main.py
+++ tp3_severini/main.py
@@ -9,7 +9,7 @@ MODO_ADJUNTAR = "a"
 MODO_ESCRIBIR ="w"
 DELIMITADOR = ";"
 ACCION_PEDIDO  = 1
-CANT_PEDIDO = 2
+CANT_VERDURA_PEDIDO = 2
 VERDURA_PEDIDO = 3
 NOMBRE_PEDIDO = 4
 TOMATE = 'T'
@@ -17,7 +17,9 @@ BROCOLI = 'B'
 ZANAHORIA = 'Z'
 LECHUGA = 'L'
 
-
+NUMERO_PEDIDO = 2
+CANT_VERDURA_MODIFICAR = 3
+VERDURA_MODIFICAR = 4
 
 def buscar_id(nombre_ingresado):
   id = 1
@@ -59,7 +61,7 @@ def agregar(pedido):
 
   else:
     with open(PEDIDOS, MODO_ADJUNTAR) as lista_pedidos, open(CLIENTES, MODO_ADJUNTAR) as lista_clientes: 
-      lista_pedidos.write(f"{id}{DELIMITADOR}{pedido[VERDURA_PEDIDO]}{DELIMITADOR}{pedido[CANT_PEDIDO]}\n")
+      lista_pedidos.write(f"{id}{DELIMITADOR}{pedido[VERDURA_PEDIDO]}{DELIMITADOR}{pedido[CANT_VERDURA_PEDIDO]}\n")
       if not se_encontro_id:
         lista_clientes.write(f"{id}{DELIMITADOR}{pedido[4]}\n")
       print("Se agrego un producto a la lista")
@@ -82,19 +84,20 @@ def modificar(pedido):
   escritor_lista_pedidos_aux = csv.writer(archivo_pedidos_aux, delimiter = DELIMITADOR)
 
   for fila in lector_lista_pedidos:
-    if fila[0] == pedido[2]:
-      if fila[1] == pedido[4] and frutas_venta(pedido[4]):
-        if pedido[3] == 0:
-          fila = ""
-        elif pedido[3] != 0:
-          fila[2] = fila[2].replace(fila[2], pedido[3])
+    if fila[0] == pedido[NUMERO_PEDIDO]:
+      if fila[1] == pedido[VERDURA_MODIFICAR] and frutas_venta(pedido[VERDURA_MODIFICAR]):
+        if pedido[CANT_VERDURA_MODIFICAR] == "0":
+          
+          print("Este pedido fue eliminado ")
+        elif pedido[CANT_VERDURA_MODIFICAR] != "0":
+          fila[2] = fila[2].replace(fila[2], pedido[CANT_VERDURA_MODIFICAR])
         print("La modificacion fue exitosa")
-      else:
-        print("Verdura ingresada incorrecta. La lista de verduras a la venta es:")
-        print("("+TOMATE+") Tomate\n("+BROCOLI+") Brocoli\n("+ZANAHORIA+") Zanahoria\n("+LECHUGA+") Lechuga")
-
-    else:
-      print("La id a la cual estas que estas queriendo hacerder no existe")
+      # else:
+      #   print("Verdura ingresada incorrecta. La lista de verduras a la venta es:")
+      #   print("("+TOMATE+") Tomate\n("+BROCOLI+") Brocoli\n("+ZANAHORIA+") Zanahoria\n("+LECHUGA+") Lechuga")
+    # else:
+    #   print("La id a la cual estas que estas queriendo hacerder no existe")
+      
     escritor_lista_pedidos_aux.writerow(fila)
 
   archivo_pedidos.close()
